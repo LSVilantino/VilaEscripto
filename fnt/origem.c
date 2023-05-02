958fc45d61a1360667229b9fd3296cc8a424a675
Cristian Silva Vieira <vilantino@tutanota.com>
2023-05-02 22:00:19 +0100

    Letras coloridas para a TUI futura.

--- fnt/origem.c
+++ fnt/origem.c
@@ -155,6 +155,10 @@ void mapa_introduzir(Mapa** mapa, Mapa valôr) {
 
     Mapa* mappa = (*mapa);
 
+    // sizeof / sizeof para pegar a quantidade de elementos
+
+    size_t elementosQuantidade = sizeof(mappa) / sizeof(mappa[0]);
+
     while (mappa[i].i == i) {
         i = i + 1;
     }
@@ -192,8 +196,21 @@ Mapa* mapa_procurar__s(Tipo tipo, void* procura, Mapa* mapa) {
 }
 
 
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
 int main(int** a, char** b) {
     setlocale(LC_CTYPE, "pt_PT.UTF-8");
+
+    // Set output mode to handle virtual terminal sequences
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE) { return GetLastError(); }
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode)) { return GetLastError(); }
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    if (!SetConsoleMode(hOut, dwMode)) { return GetLastError(); }
 
     printf("");
 
@@ -229,8 +246,25 @@ int main(int** a, char** b) {
 
         mapa_introduzir(&mapa, conteúdo);
 
+
+        system("cls");
+        // Try some Set Graphics Rendition (SGR) terminal escape sequences
+        wprintf(L"\x1b[31mThis text has a red foreground using SGR.31.\r\n");
+        wprintf(L"\x1b[1mThis text has a bright (bold) red foreground using SGR.1 to affect the previous color setting.\r\n");
+        wprintf(L"\x1b[mThis text has returned to default colors using SGR.0 implicitly.\r\n");
+        wprintf(L"\x1b[34;46mThis text shows the foreground and background change at the same time.\r\n");
+        wprintf(L"\x1b[0mThis text has returned to default colors using SGR.0 explicitly.\r\n");
+        wprintf(L"\x1b[31;32;33;34;35;36;101;102;103;104;105;106;107mThis text attempts to apply many colors in the same command. Note the colors are applied from left to right so only the right-most option of foreground cyan (SGR.36) and background bright white (SGR.107) is effective.\r\n");
+        wprintf(L"\x1b[39mThis text has restored the foreground color only.\r\n");
+        wprintf(L"\x1b[49mThis text has restored the background color only.\r\n");
+
         Mapa* mapaEncontrado = mapa_procurar__s(tipo_char, "caminho", mapa);
-        printf("%s %s %d\n", (char*)mapaEncontrado[0].passe, (char*)mapaEncontrado[0].valôr, mapaEncontrado[0].i);
+
+        wprintf(L"\x1b[34;46m%S %S %d\r\n", (char*)mapaEncontrado[0].passe, (char*)mapaEncontrado[0].valôr, mapaEncontrado[0].i);
+
+        while (1) {
+
+        }
 
         n = n + 1;
     }
